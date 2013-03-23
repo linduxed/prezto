47dc5acbc459bd8546bb949ee646caa46df3c835
linduxed <linduxed@gmail.com>
2013-03-23 22:31:40 +0100

    Fixed Termcap colors.
    Colors didn't work because ":omz:..." was used instead of ":prezto:...".
    
    Also made the comments identical to the ones found in the main repo.

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -44,13 +44,13 @@ fi
 # Termcap
 #
 
-if zstyle -t ':omz:environment:termcap' color; then
-  export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-  export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
-  export LESS_TERMCAP_me=$'\E[0m'           # end mode
-  export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-  export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
-  export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-  export LESS_TERMCAP_us=$'\E[04;32;5;146m' # begin underline
+if zstyle -t ':prezto:environment:termcap' color; then
+  export LESS_TERMCAP_mb=$'\E[01;31m'       # Begins blinking.
+  export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # Begins bold.
+  export LESS_TERMCAP_me=$'\E[0m'           # Ends mode.
+  export LESS_TERMCAP_se=$'\E[0m'           # Ends standout-mode.
+  export LESS_TERMCAP_so=$'\E[38;5;246m'    # Begins standout-mode.
+  export LESS_TERMCAP_ue=$'\E[0m'           # Ends underline.
+  export LESS_TERMCAP_us=$'\E[04;32;5;146m' # Begins underline.
 fi
 
