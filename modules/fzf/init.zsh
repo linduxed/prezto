e6432eb5fb37b235a603a68e65665d2dc92f91f3
Mikael Muszynski <linduxed@gmail.com>
2016-06-01 13:03:51 +0200

    Make fzf module load right files
    Apparently /etc/profile.d/fzf.zsh isn't a thing anymore.

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -2,8 +2,9 @@
 #   linduxed <linduxed@gmail.com>
 #
 
+test -r "/usr/share/fzf/key-bindings.zsh" && . "/usr/share/fzf/key-bindings.zsh"
+
 if test -d /etc/profile.d/; then
-  test -r "/etc/profile.d/fzf.zsh" && . "/etc/profile.d/fzf.zsh"
   test -r "/etc/profile.d/fzf-extras.zsh" && . "/etc/profile.d/fzf-extras.zsh"
   test -r "/etc/profile.d/fzf-extras.bash" && . "/etc/profile.d/fzf-extras.bash"
 fi
